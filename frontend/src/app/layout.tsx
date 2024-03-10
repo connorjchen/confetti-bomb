52c04c979bb4e5cc1290dbecc58b7991f57b6102
Connor Chen <connorjchen2784@gmail.com>
2024-03-09 21:42:32 -0500

    feat: reset back to create-next-app

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Confetti Bomb",
+  description:
+    "Confetti Bomb is a fun and exciting way to celebrate any accomplishment. Send a confetti bomb to someone today!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
